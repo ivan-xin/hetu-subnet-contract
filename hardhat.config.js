7aac58997e5ba097c27fb5a25100fd9685f634d2
ivan <ivan.coder.zx@gmail.com>
2025-07-10 15:12:41 +0800

    update project dependencies

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+// 您提供的私钥
+const PRIVATE_KEY = "0x3272afed0bf366b6a7cf889543a320158ec25cd43760ff5b6fe7fb15bd3f234d";
+// 您提供的RPC地址
+const CUSTOM_RPC_URL = "https://rpc.testchainv1.hetuscan.com";
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    // 本地开发网络
+    hardhat: {
+      // 本地网络配置
+    },
+    // 您的自定义开发链
+    customchain: {
+      url: CUSTOM_RPC_URL,
+      accounts: [PRIVATE_KEY],
+      chainId: 560000, // 您提供的Chain ID
+    },
+  },
 };
